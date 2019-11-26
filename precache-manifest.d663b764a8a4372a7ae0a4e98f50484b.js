85947d2b3c1bf397b0ed0a57e43142d5f78e15d1
Julie Norcross <julesnorc@gmail.com>
2019-11-26 10:03:54 -0500

    Updates

---
+++ precache-manifest.d663b764a8a4372a7ae0a4e98f50484b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9222935b3072343a15b29bc61a6da509",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "5e249c502aa77303e74e",
+    "url": "/Pokedex/static/css/main.6bb7ab75.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Pokedex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "5e249c502aa77303e74e",
+    "url": "/Pokedex/static/js/main.e3c72f28.chunk.js"
+  },
+  {
+    "revision": "119fa3083960fadc0325",
+    "url": "/Pokedex/static/js/runtime~main.cf3900f6.js"
+  }
+]);
